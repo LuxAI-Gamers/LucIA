3eb3416c67d2687d8a2d27a758eef335d9394788
DelgadoPanadero <angel.sherpa80@gmail.com>
2021-08-26 00:51:36 +0200

    feat(): initial commit

---
+++ agent.py
@@ -0,0 +1,31 @@
+from lux.game import Game
+from lux.game_map import Cell, RESOURCE_TYPES
+from lux.constants import Constants
+from lux.game_constants import GAME_CONSTANTS
+from lux import annotate
+import math
+
+# we declare this global game_state object so that state persists across turns so we do not need to reinitialize it all the time
+game_state = None
+
+# this is the basic agent definition. At the moment this agent does nothing (and actually will survive for a bit before getting consumed by darkness)
+def agent(observation, configuration):
+    global game_state
+
+    ### Do not edit ###
+    if observation["step"] == 0:
+        game_state = Game()
+        game_state._initialize(observation["updates"])
+        game_state._update(observation["updates"][2:])
+        game_state.id = observation.player
+    else:
+        game_state._update(observation["updates"])
+    
+    actions = []
+
+    ### AI Code goes down here! ### 
+    player = game_state.players[observation.player]
+    opponent = game_state.players[(observation.player + 1) % 2]
+    width, height = game_state.map.width, game_state.map.height
+
+    return actions

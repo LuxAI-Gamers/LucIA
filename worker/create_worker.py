654e91d8ae90d156c79e8422dfd9837519c15010
DelgadoPanadero <angel.sherpa80@gmail.com>
2021-08-30 13:33:24 +0200

    fix(): can_build task

--- worker/create_worker.py
+++ worker/create_worker.py
@@ -1,5 +1,3 @@
-from typing import BinaryIO
-from worker.tasks import build_citytile
 from .tasks import CanAct
 from .tasks import Pillage
 from .tasks import IsCargoFull
@@ -11,23 +9,26 @@ from .tasks import BuildCityTile
 
 from bh_trees import Inverter, Sequence, Selector
 
-def create_dumb_worker():
+def create_simple_worker():
 
     root = Selector()
 
     # 1st level
     first = root.add_child(Inverter())
     back_to_city_seq = root.add_child(Sequence())
-    collect_seq = root.add_child(Sequence())
     build_city_seq = root.add_child(Sequence())
+    collect_seq = root.add_child(Sequence())
 
     # 2nd level
     first.add_child(CanAct())
+
     back_to_city_seq.add_child(IsCargoFull())
     back_to_city_seq.add_child(FindNearestCity())
     back_to_city_seq.add_child(MoveToPosition())
+
     collect_seq.add_child(FindNearestResource())
     collect_sel = collect_seq.add_child(Selector())
+
     build_city_seq.add_child(CanBuildCityTile())
     build_city_seq.add_child(BuildCityTile())
 
--- worker/tasks/can_build_citytile.py
+++ worker/tasks/can_build_citytile.py
@@ -11,4 +11,5 @@ class CanBuildCityTile(Task):
 
     def run(self):
         object = self._blackboard.get_value('object')
-        return True if object.can_build() else False
+        game_map = self._blackboard.get_value('map')
+        return True if object.can_build(game_map) else False

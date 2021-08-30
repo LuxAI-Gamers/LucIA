04abd5e24fcc8f43a72e919af2ea9d55ddab96d4
DelgadoPanadero <angel.sherpa80@gmail.com>
2021-08-30 18:35:27 +0200

    feat(): update worker graph

--- worker/create_worker.py
+++ worker/create_worker.py
@@ -16,7 +16,6 @@ def create_simple_worker():
     # 1st level
     first = root.add_child(Inverter())
     back_to_city_seq = root.add_child(Sequence())
-    build_city_seq = root.add_child(Sequence())
     collect_seq = root.add_child(Sequence())
 
     # 2nd level
@@ -24,16 +23,21 @@ def create_simple_worker():
 
     back_to_city_seq.add_child(IsCargoFull())
     back_to_city_seq.add_child(FindNearestCity())
-    back_to_city_seq.add_child(MoveToPosition())
+    select_action = back_to_city_seq.add_child(Selector())
 
     collect_seq.add_child(FindNearestResource())
     collect_sel = collect_seq.add_child(Selector())
 
-    build_city_seq.add_child(CanBuildCityTile())
-    build_city_seq.add_child(BuildCityTile())
 
     # 3rd level
+    build_city_seq = select_action.add_child(Sequence())
+    select_action.add_child(MoveToPosition())
+
     collect_sel.add_child(MoveToPosition())
     collect_sel.add_child(Pillage())
+
+    # 4th level
+    build_city_seq.add_child(CanBuildCityTile())
+    build_city_seq.add_child(BuildCityTile())
 
     return root
---
+++ worker/tasks/should_build_citytile.py
@@ -0,0 +1,22 @@
+import sys
+import math
+
+from bh_trees import Task
+
+
+class CanBuildCityTile(Task):
+
+    def __init__(self):
+        super(CanBuildCityTile, self).__init__()
+
+    def run(self):
+        # Number of cities equal to number of workers
+
+        # Reduce city cost
+
+        # City near resource
+
+
+        object = self._blackboard.get_value('object')
+        game_map = self._blackboard.get_value('map')
+        return True if object.can_build(game_map) else False

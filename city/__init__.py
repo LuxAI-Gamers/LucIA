427528d51914f659b9927dd1dbd00d2a71bd6efd
DelgadoPanadero <angel.sherpa80@gmail.com>
2021-09-05 12:18:36 +0200

    refactor(#32): worker for lucIA v2

--- city/__init__.py
+++ city/__init__.py
@@ -1,2 +1,2 @@
 from .create_city import create_night_city
-from .create_city import create_simple_city
+from .create_city import create_day_city
--- worker/tasks/pillage.py
+++ worker/tasks/pillage.py
@@ -9,9 +9,9 @@ class Pillage(Task):
 
     def run(self):
         map = self._blackboard.get_value('map')
-        object = self._blackboard.get_value('object')        
+        object = self._blackboard.get_value('object')
 
-        if self.is_in_resource(object, map):
+        if self.is_in_resource(object, map) and object.get_cargo_space_left()>0:
             pillage = object.pillage()
             self._blackboard.append_values(actions=pillage)
             return True
